9b728a5835ab2d445a96e283020007fdf6c44e08
Dmitry Russkikh <cyradin@protonmail.com>
2026-03-12 00:05:45 +0300

    Ping database in /readyz handler

--- internal/router/debug.go
+++ internal/router/debug.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/cyradin/fixik/internal/container"
 	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func NewDebug(_ *container.Container) *chi.Mux {
+func NewDebug(container *container.Container) *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Get("/livez", livenessHandler)
-	r.Get("/readyz", readinessHandler)
+	r.Get("/readyz", readinessHandler(container.PgPool()))
 
 	return r
 }
@@ -20,6 +21,14 @@ func livenessHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func readinessHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+func readinessHandler(pgPool *pgxpool.Pool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := pgPool.Ping(r.Context()); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
 }
