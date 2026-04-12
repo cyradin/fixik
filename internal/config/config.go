04a2146440f5860636e119a1fa2508db685bd8a9
Dmitry Russkikh <cyradin@protonmail.com>
2026-04-12 21:46:31 +0300

    Set default config values

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,37 +17,37 @@ type Config struct {
 
 //nolint:gosec
 type AuthConfig struct {
-	Secret          string        `envconfig:"FIXIK_AUTH_SECRET" required:"true"`
-	AccessTokenTTL  time.Duration `envconfig:"FIXIK_AUTH_ACCESS_TOKEN_TTL" required:"true"`
-	RefreshTokenTTL time.Duration `envconfig:"FIXIK_AUTH_REFRESH_TOKEN_TTL" required:"true"`
-	SecureCookies   bool          `envconfig:"FIXIK_AUTH_SECURE_COOKIES" required:"true"`
+	Secret          string        `envconfig:"FIXIK_AUTH_SECRET" required:"true" default:"secret"`
+	AccessTokenTTL  time.Duration `envconfig:"FIXIK_AUTH_ACCESS_TOKEN_TTL" required:"true" default:"15m"`
+	RefreshTokenTTL time.Duration `envconfig:"FIXIK_AUTH_REFRESH_TOKEN_TTL" required:"true" default:"672h"` // default - 28 days
+	SecureCookies   bool          `envconfig:"FIXIK_AUTH_SECURE_COOKIES" required:"true" default:"false"`
 }
 
 type HTTPServerConfig struct {
-	Addr              string        `envconfig:"FIXIK_HTTP_SERVER_ADDR" required:"true"`
-	ReadHeaderTimeout time.Duration `envconfig:"FIXIK_HTTP_SERVER_READ_HEADER_TIMEOUT" required:"true"`
-	ReadTimeout       time.Duration `envconfig:"FIXIK_HTTP_SERVER_READ_TIMEOUT" required:"true"`
-	WriteTimeout      time.Duration `envconfig:"FIXIK_HTTP_SERVER_WRITE_TIMEOUT" required:"true"`
-	IdleTimeout       time.Duration `envconfig:"FIXIK_HTTP_SERVER_IDLE_TIMEOUT" required:"true"`
-	AllowedOrigins    []string      `envconfig:"FIXIK_HTTP_SERVER_ALLOWED_ORIGINS" required:"true"`
+	Addr              string        `envconfig:"FIXIK_HTTP_SERVER_ADDR" required:"true" default:":8080"`
+	ReadHeaderTimeout time.Duration `envconfig:"FIXIK_HTTP_SERVER_READ_HEADER_TIMEOUT" required:"true" default:"5s"`
+	ReadTimeout       time.Duration `envconfig:"FIXIK_HTTP_SERVER_READ_TIMEOUT" required:"true" default:"10s"`
+	WriteTimeout      time.Duration `envconfig:"FIXIK_HTTP_SERVER_WRITE_TIMEOUT" required:"true" default:"10s"`
+	IdleTimeout       time.Duration `envconfig:"FIXIK_HTTP_SERVER_IDLE_TIMEOUT" required:"true" default:"60s"`
+	AllowedOrigins    []string      `envconfig:"FIXIK_HTTP_SERVER_ALLOWED_ORIGINS" required:"true" default:"http://localhost:8081"`
 }
 
 type HTTPDebugServerConfig struct {
-	Addr              string        `envconfig:"FIXIK_HTTP_DEBUG_SERVER_ADDR" required:"true"`
-	ReadHeaderTimeout time.Duration `envconfig:"FIXIK_HTTP_DEBUG_SERVER_READ_HEADER_TIMEOUT" required:"true"`
-	ReadTimeout       time.Duration `envconfig:"FIXIK_HTTP_DEBUG_SERVER_READ_TIMEOUT" required:"true"`
-	WriteTimeout      time.Duration `envconfig:"FIXIK_HTTP_DEBUG_SERVER_WRITE_TIMEOUT" required:"true"`
-	IdleTimeout       time.Duration `envconfig:"FIXIK_HTTP_DEBUG_SERVER_IDLE_TIMEOUT" required:"true"`
+	Addr              string        `envconfig:"FIXIK_HTTP_DEBUG_SERVER_ADDR" required:"true" default:":8081"`
+	ReadHeaderTimeout time.Duration `envconfig:"FIXIK_HTTP_DEBUG_SERVER_READ_HEADER_TIMEOUT" required:"true" default:"5s"`
+	ReadTimeout       time.Duration `envconfig:"FIXIK_HTTP_DEBUG_SERVER_READ_TIMEOUT" required:"true" default:"10s"`
+	WriteTimeout      time.Duration `envconfig:"FIXIK_HTTP_DEBUG_SERVER_WRITE_TIMEOUT" required:"true" default:"10s"`
+	IdleTimeout       time.Duration `envconfig:"FIXIK_HTTP_DEBUG_SERVER_IDLE_TIMEOUT" required:"true" default:"60s"`
 	// Куда ходит веб-интерфейс сваггера в Try it out
-	SwaggerAddr string `envconfig:"FIXIK_HTTP_DEBUG_SWAGGER_ADDR" required:"true"`
+	SwaggerAddr string `envconfig:"FIXIK_HTTP_DEBUG_SWAGGER_ADDR" required:"true" default:"localhost:8080"`
 }
 
 type LogConfig struct {
-	Level string `envconfig:"FIXIK_LOG_LEVEL" required:"true"`
+	Level string `envconfig:"FIXIK_LOG_LEVEL" required:"true" default:"info"`
 }
 
 type PostgresConfig struct {
-	URL string `envconfig:"FIXIK_POSTGRES_URL" required:"true"`
+	URL string `envconfig:"FIXIK_POSTGRES_URL" required:"true" default:"postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"`
 }
 
 func New() (*Config, error) {
